911433073ac46a4d419429b9596b8e87d57b538a
AbolfazlAdhami <aadhami880@gmail.com>
2025-10-04 17:28:24 +0330

    change names and dir

---
+++ express002/src/index.mjs
@@ -0,0 +1,12 @@
+import express from "express";
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/", (req, res) => {});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Running on Port ${PORT}`);
+});
