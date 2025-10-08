fdc72f2afd537f6ae63494e8888529857d5a20e3
AbolfazlAdhami <aadhami880@gmail.com>
2025-10-09 02:21:30 +0330

    init: 002 user contoller and middlawre

--- express002/src/controller/users.mjs
+++ express002/src/controller/users.mjs
@@ -1,1 +1,8 @@
-import {} from "express-validator"
+import { matchedData, validationResult } from "express-validator";
+import { users } from "../data/users.mjs";
+import { hashPassword } from "../utils/helpers.mjs";
+import { User } from "../schemas/user";
+
+export const getUserByIdHandler = (req, res, next) => {};
+
+export const createUserHandler = async (req, res, next) => {};
