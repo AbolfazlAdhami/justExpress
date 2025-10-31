ed63010687b0db5953bdbe5174ffb608f020bb79
AbolfazlAdhami <aadhami880@gmail.com>
2025-10-31 23:56:54 +0330

    [config] add path config in global configs

--- express003/modules/config.js
+++ express003/modules/config.js
@@ -1,3 +1,13 @@
+const path = require("path");
+
 module.exports = {
   port: 8000,
+  path: {
+    controllers: {
+      api: path.resolve("./controllers/api"),
+      web: path.resolve("./controllers/web"),
+    },
+    model: path.resolve("./models"),
+    controller: path.resolve("./controllers"),
+  },
 };
