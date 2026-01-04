38115ef529fa4e4882316edbb0a39bcc1764dbaa
AbolfazlAdhami <aadhami880@gmail.com>
2026-01-04 23:50:24 +0330

    [feat&fix]: user transform and fix path

---
+++ express003/modules/transfomrs/v1/UserTransform.js
@@ -0,0 +1,20 @@
+const Transform = require("../Transform");
+const jwt = require("jsonwebtoken");
+
+module.exports = class UserTransform extends Transform {
+  transform(item, createToken = false) {
+    this.createToken = createToken;
+    return {
+      name: this.name,
+      email: this.email,
+      ...this.withToken(item),
+    };
+  }
+  withToken(item) {
+    if (item.token) return { token: item.token };
+
+    if(item.createToken){
+      let token=jwt.sign()
+    }
+  }
+};
