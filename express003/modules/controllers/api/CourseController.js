2f1b36dc423e2c83b9cafc2f59bf070cdac57408
AbolfazlAdhami <aadhami880@gmail.com>
2026-01-09 11:23:51 +0330

    feat: course countroller done

---
+++ express003/modules/controllers/api/CourseController.js
@@ -0,0 +1,20 @@
+const config = require("../../config");
+
+const Controller = require(`${config.path.controller}/Controller`);
+const CourseTransform = require(`${config.path.transforms}/v1/CourseTransform`);
+
+module.exports = new (class CourseController extends Controller {
+  index(req, res) {
+    this.model.Course.find({}, (err, courses) => {
+      if (err) throw err;
+
+      if (courses)
+        return res.json({
+          data: new CourseTransform().transforms(courses),
+          success: true,
+        });
+
+      res.json({ message: "Courses empty ", success: false });
+    });
+  }
+})();
