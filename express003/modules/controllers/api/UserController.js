6d462ec63bc7eab6bce8fad5539cb942b472f7a6
AbolfazlAdhami <aadhami880@gmail.com>
2026-01-09 07:51:37 +0330

    feat: user controller done

---
+++ express003/modules/controllers/api/UserController.js
@@ -0,0 +1,28 @@
+const config = require("../../config");
+const Controller = require(`${config.path.controllers}/Controller`);
+const UserTransform = require(`${config.path.transforms}/v1/UserTransform`);
+
+module.exports = new (class UserController extends Controller {
+  index(req, res) {
+    return res.json({
+      data: new UserTransform().transform(req.user),
+    });
+  }
+  uploadImage(req, res) {
+    res.json(req.file);
+    if (req.file) {
+      res.json({
+        message: "File Uploaded Successfully",
+        data: {
+          imagePath: `http://localhost:8080/${req.file.path.replace(/\\/g, "/")}`,
+        },
+        success: true,
+      });
+    } else {
+      res.json;
+      res.json({
+        message: "File Could not upload!",
+      });
+    }
+  }
+})();
