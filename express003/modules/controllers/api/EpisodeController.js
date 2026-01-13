bd1925a12d4de1b25b3067a6d6062592482b89fb
AbolfazlAdhami <aadhami880@gmail.com>
2026-01-13 19:42:22 +0330

    feat: Episode controller comleted

--- express003/modules/controllers/api/EpisodeController.js
+++ express003/modules/controllers/api/EpisodeController.js
@@ -1,2 +1,23 @@
 const config = require("../../config");
 
+const Controller = require(`../Controller`);
+const EpisodeTransform = require(`${config.path.controller}/EpisodeTransform`);
+const CourseTransform = require(`${config.path.controller}/CourseTransform`);
+
+module.exports = new (class EpisodeController extends Controller {
+  single(req, res) {
+    req.checkParams("id", "id is invalid ").isMongoId();
+
+    if (this.showValidationErrors(req, res)) return;
+
+    this.model.Episode.findById(req, params.id)
+      .populate("course")
+      .exec((err, episode) => {
+        if (err) throw err;
+
+        if (episode) return req.json({ data: new EpisodeTransform().transform(episode), success: true });
+
+        return res.json({ message: "episode empty", success: false });
+      });
+  }
+})();
